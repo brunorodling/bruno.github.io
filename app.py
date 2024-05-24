2265071479fd175d524a53d6b55321d60f7c0226
brunorodling <131053529+brunorodling@users.noreply.github.com>
2024-05-23 23:39:48 -0300

    port modified

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def attack():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
 
